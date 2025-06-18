9e19cc975015b91d5f8211249f5659b4549a07d7
Wuyi Weng <wuweng@microsoft.com>
2025-06-18 00:05:51 -0700

    set up sample solution folder

---
+++ src/ResourceManagementTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ResourceManagementTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true, "This test should always pass.");
+        }
+    }
+}
