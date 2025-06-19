ce07b0477352566f30594615cbb72b01b2a6d6ac
Wuyi Weng <wuweng@microsoft.com>
2025-06-18 21:37:59 -0700

    Add AzureTestBase enhancements and new unit test for Azure connection verification

---
+++ src/ResourceManagementTests/SampleAzureUnitTest1.cs
@@ -0,0 +1,31 @@
+using TestUtility;
+using Xunit.Abstractions;
+
+namespace ResourceManagementTests
+{
+    public class SampleAzureUnitTest1 : AzureTestBase
+    {
+        public SampleAzureUnitTest1(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task VerifyAzureConnection()
+        {
+            string uniqueString = Guid.NewGuid().ToString();
+            Logger.Information("This is a test log message. " + uniqueString);
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                await client.GetAsync($"https://invalid-domain-value-{uniqueString}.com");
+            }
+            catch { }
+
+            var logEvents = GetLogEvents();
+            Assert.True(logEvents.Any(e => e.MessageTemplate.Text.Contains(uniqueString)), "Log message should contain the unique string.");
+            Assert.True(true, "This test should always pass.");
+        }
+    }
+}
--- src/TestUtility/AzureTestBase.cs
+++ src/TestUtility/AzureTestBase.cs
@@ -1,10 +1,53 @@
-﻿using System;
+﻿using Azure.Core;
+using Azure.Identity;
+using Serilog.Core;
+using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
+using Xunit.Abstractions;
 
 namespace TestUtility
 {
-    public class AzureTestBase
+    public class AzureTestBase : TestBase
     {
+        private const string AzureUnitTestSubscriptionId = nameof(AzureUnitTestSubscriptionId);
+
+        public AzureTestBase(ITestOutputHelper output, [CallerFilePath] string sourceFile = "")
+            : base(output, useAppInsights: true, sourceFile)
+        {
+            SubscriptionId = GetUnitTestSubscriptionId();
+            TokenCredential = LoadTestCredential();
+        }
+
+        public string SubscriptionId { get; }
+
+        public TokenCredential TokenCredential { get; }
+
+        private TokenCredential LoadTestCredential()
+        {
+            Logger.Information("unit_test_auth_setup. Using az cli credential.");
+
+            var tokenCredential = new AzureCliCredential();
+
+            return tokenCredential;
+        }
+
+        private static string GetUnitTestSubscriptionId()
+        {
+            string subscriptionIdStr = Environment.GetEnvironmentVariable(AzureUnitTestSubscriptionId);
+
+            if (string.IsNullOrEmpty(subscriptionIdStr))
+            {
+                throw new InvalidOperationException($"Cannot find the subscription id for running the unit tests. It should be set in the environment variable with name {AzureUnitTestSubscriptionId}.");
+            }
+
+            if (!Guid.TryParse(subscriptionIdStr, out _))
+            {
+                throw new InvalidOperationException($"The unit test default subscription id {subscriptionIdStr} is not a valid GUID.");
+            }
+
+            return subscriptionIdStr;
+        }
     }
 }
