80abd9fff1e43e4341a79fe71817354702b3ff33
Wuyi Weng <wuweng@microsoft.com>
2025-06-18 22:45:12 -0700

    Enhance AzureTestBase with resource group creation and disposal logic; update TestBase to include timestamp property

--- src/TestUtility/AzureTestBase.cs
+++ src/TestUtility/AzureTestBase.cs
@@ -1,28 +1,95 @@
-﻿using Azure.Core;
+﻿using Azure;
+using Azure.Core;
 using Azure.Identity;
-using Serilog.Core;
+using Azure.ResourceManager.Resources;
 using System;
-using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using System.Text;
 using Xunit.Abstractions;
+using Azure.ResourceManager;
 
 namespace TestUtility
 {
     public class AzureTestBase : TestBase
     {
+        private static readonly Random s_rand = new Random();
+
         private const string AzureUnitTestSubscriptionId = nameof(AzureUnitTestSubscriptionId);
 
-        public AzureTestBase(ITestOutputHelper output, [CallerFilePath] string sourceFile = "")
+        public AzureTestBase(ITestOutputHelper output, bool createResourceGroup = true, [CallerFilePath] string sourceFile = "")
             : base(output, useAppInsights: true, sourceFile)
         {
             SubscriptionId = GetUnitTestSubscriptionId();
+
             TokenCredential = LoadTestCredential();
+
+            ArmClient = new ArmClient(TokenCredential);
+
+            DefaultSubscription = ArmClient.GetSubscriptionResource(SubscriptionResource.CreateResourceIdentifier(SubscriptionId));
+
+            ResourceGroupName = $"{TestClassName}-{TestLocation.Name}-{DateTimeStr}-{s_rand.Next(0, 999)}";
+
+            if (createResourceGroup)
+            {
+                try
+                {
+                    ResourceGroupCollection resourceGroups = DefaultSubscription.GetResourceGroups();
+
+                    ResourceGroupData resourceGroupData = new ResourceGroupData(TestLocation);
+
+                    var operation = resourceGroups.CreateOrUpdate(WaitUntil.Completed, ResourceGroupName, resourceGroupData);
+
+                    TestResourceGroup = operation.Value;
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex, "err_create_rg");
+                    throw new InvalidOperationException($"Failed at creating testing resource group with name '{ResourceGroupName}' in subscription '{SubscriptionId}'. There might be testing credential issues. Please ensure you have done 'az login' before.", ex);
+                }
+            }
         }
+
+        public ArmClient ArmClient { get; }
 
         public string SubscriptionId { get; }
 
+        public SubscriptionResource DefaultSubscription { get; }
+
+        public AzureLocation TestLocation => AzureLocation.WestUS2;
+
+        public string ResourceGroupName { get; }
+
+        public ResourceGroupResource TestResourceGroup { get; }
+
         public TokenCredential TokenCredential { get; }
+
+        public override void Dispose()
+        {
+            bool deleteResourceGroup = true;
+
+            if (TestResourceGroup == null)
+            {
+                deleteResourceGroup = false;
+            }
+            else if (IsFailure == true)
+            {
+                deleteResourceGroup = false;
+            }
+
+            if (deleteResourceGroup)
+            {
+                try
+                {
+                    TestResourceGroup.DeleteAsync(WaitUntil.Started).GetAwaiter().GetResult();
+                }
+                catch
+                {
+                }
+            }
+
+            base.Dispose();
+
+            GC.SuppressFinalize(this);
+        }
 
         private TokenCredential LoadTestCredential()
         {
